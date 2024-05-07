25ea9480c6c0c01ad96b020dc8cd43e45bef5ae1
DevDev202 <you@example.com>
2024-05-07 16:54:35 +0300

    Практическое задание по теме "Списки и словари"

---
+++ 2023_09_25_Dictionaries/homework5.py
@@ -0,0 +1,13 @@
+my_list = ["apple", "banana", "orange", "kiwi"]
+print(my_list)
+print("First element: ", my_list[0])
+print("Last element: ", my_list[-1])
+print("Sublist: ", my_list[2:3])
+my_list[2] = "grape"
+print("Modified dictionary: ", my_list)
+
+my_dict = {"apple" : "яблоко", "banana" : "банан", "orange" : "апельсин", "kiwi" : "киви"}
+print("Dictionary", my_dict)
+print("Translation: ", my_dict.get("orange"))
+my_dict.update({"kiwi" : "киви"})
+print("Modified: ", my_dict)
