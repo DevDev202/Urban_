6948073efd954c4cb0401fb48de35d3496d75abb
DevDev202 <you@example.com>
2024-05-08 11:41:00 +0300

    Внесены исправлени ошибок.

--- 2023_09_25_Dictionaries/homework5.py
+++ 2023_09_25_Dictionaries/homework5.py
@@ -2,7 +2,7 @@ my_list = ["apple", "banana", "orange", "kiwi"]
 print("List: ", my_list)
 print("First element: ", my_list[0])
 print("Last element: ", my_list[-1])
-print("Sublist: ", my_list[2:3])
+print("Sublist: ", my_list[2:5])
 my_list[2] = "grape"
 print("Modified list: ", my_list)
 
